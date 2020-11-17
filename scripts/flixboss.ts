7a7365bde128cf09cd80dd98c4d0a4979f23961b
Finn Pauls <mbp@pauls.dev>
2020-11-17 17:41:26 +0000

    Fetch more movies

--- scripts/flixboss.ts
+++ scripts/flixboss.ts
@@ -1,62 +1,56 @@
-import flixboss from '../data/flixboss.json'
 import {JSDOM} from 'jsdom'
 import {save} from './util'
+import got from 'got'
 
-const document = new JSDOM(flixboss.data).window.document
+async function main() {
+  const movies = await fetchFlixboss()
+  console.log('Found', movies.length, 'movies')
+  save('movies', movies)
+}
 
-const movies = []
+main()
 
-document.querySelectorAll('.title').forEach((element) => {
-  const id = element.getAttribute('data-id')
+async function fetchFlixboss() {
+  let movies = []
 
-  const image = element
-    .querySelector('.img')
-    .getAttribute('style')
-    .slice("background-image: url('".length, -2)
+  for (let i = 1; i < 20; i++) {
+    const {data} = await got(
+      'https://de.flixboss.com/x/filter/filme/-/-/imdb/2/0/1'
+    ).json()
 
-  const title = element.querySelector('h4').innerHTML
+    movies = movies.concat(extractMovies(data))
+  }
 
-  const year = Number(element.querySelector('.year').innerHTML)
+  return movies
+}
 
-  const genres = []
+function extractMovies(flixBossPage: string) {
+  const document = new JSDOM(flixBossPage).window.document
 
-  element.querySelectorAll('.genre').forEach((genre) => {
-    genres.push(genre.innerHTML)
+  const movies = []
+
+  document.querySelectorAll('.title').forEach((element) => {
+    const id = element.getAttribute('data-id')
+
+    const image = element
+      .querySelector('.img')
+      .getAttribute('style')
+      .slice("background-image: url('".length, -2)
+
+    const title = element.querySelector('h4').innerHTML
+
+    const year = Number(element.querySelector('.year').innerHTML)
+
+    const genres = []
+
+    element.querySelectorAll('.genre').forEach((genre) => {
+      genres.push(genre.innerHTML)
+    })
+
+    const rating = Number(element.querySelector('.rating span').innerHTML)
+
+    movies.push({id, title, year, genres, rating, image})
   })
 
-  const rating = Number(element.querySelector('.rating span').innerHTML)
-
-  movies.push({id, title, year, genres, rating, image})
-})
-
-save('movies', movies)
-
-/*
-<li class="title" data-id="70005379">
-
-<a class="item " href="/film/die-verurteilten-70005379" data-url="die-verurteilten-70005379">
-
-	<div class="img lazy" style="background-image: url('https://c.flikshost.com/70005379/backdrops/small/xBKGJQsAIeweesB79KC89FpBrVr.jpg')"></div>
-
-	<div class="info">
-		<h4>Die Verurteilten</h4>
-
-		<ul class="year-genres">
-							<li class="year">1994</li>
-
-										<li class="genre">Krimi</li>
-														<li class="genre">Drama</li>
-														<li class="genre">Geschichte</li>
-									</ul>
-
-					<div class="rating" style="background: #60DD22">
-				IMDb
-				<span>9.3</span>
-			</div>
-
-			</div>
-
-
-</a>
-/li>
-*/
+  return movies
+}
--- scripts/util.ts
+++ scripts/util.ts
@@ -1,5 +1,6 @@
 import {writeFileSync} from 'fs'
 import {resolve} from 'path'
+import got from 'got'
 
 export function save(name: string, data: any) {
   writeFileSync(
